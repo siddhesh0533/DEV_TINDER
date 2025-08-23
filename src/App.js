291cb8e7db66a76040713245413bf8aec839d9c0
Siddhesh <siddheshpatil0533@gmail.com>
2025-08-23 22:43:58 +0530

    playing with routes

--- src/App.js
+++ src/App.js
@@ -2,29 +2,19 @@ const express = require("express");
 
 const app = express();
 
-app.use("/test",(req, res)=>{
+app.get("/abc",(req, res)=>{
+    console.log(req.query);
+    
     res.send("hello test")
 });
 
-app.post("/", (req, res)=>{
-    res.send("/ post request ")
+app.get("/abc/:userId/:password/:age/:data",(req, res)=>{
+    console.log(req.params);
+    
+    res.send("hello test")
 });
 
-app.delete("/", (req, res)=>{
-    res.send("/ delete request ")
-});
 
-app.put("/", (req, res)=>{
-    res.send("/ put request ")
-});
-
-app.use("/get",(req, res)=>{
-    res.send("hello get")
-});
-
-app.use("/",(req, res)=>{
-    res.send("hello")
-});
 
 app.listen(7777, ()=>{
     console.log("running on port 7777");
