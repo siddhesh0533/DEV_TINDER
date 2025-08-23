b3667a0186f5da27aac69247a864cec063185c03
Siddhesh <siddheshpatil0533@gmail.com>
2025-08-24 00:48:38 +0530

    multiple route handlers

--- src/App.js
+++ src/App.js
@@ -2,21 +2,24 @@ const express = require("express");
 
 const app = express();
 
-app.get("/abc",(req, res)=>{
-    console.log(req.query);
-    
-    res.send("hello test")
-});
+app.get("/user",
+    [(req, res, next) => {
+        // res.send("hello test")
+        next();
+    }],
+    (req, res) => {
+        res.send("hell0 2")
+    }
+);
 
-app.get("/abc/:userId/:password/:age/:data",(req, res)=>{
+app.get("/abc/:userId/:password/:age/:data", (req, res) => {
     console.log(req.params);
-    
+
     res.send("hello test")
 });
 
 
 
-app.listen(7777, ()=>{
+app.listen(7777, () => {
     console.log("running on port 7777");
 })
- 
