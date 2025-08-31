7c84fd1899f03a134a520df80d0ab839c1bb16f9
Siddhesh <siddheshpatil0533@gmail.com>
2025-09-01 00:54:53 +0530

    profile/changePassword API

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -2,6 +2,7 @@ const express = require("express");
 const User = require("../model/user");
 const { UserAuth } = require("../middleware/UserAuth");
 const {validateEditData} = require("../utils/validate")
+const bcript = require("bcrypt")
 
 const profileRouter = express.Router();
 
@@ -30,6 +31,31 @@ profileRouter.patch("/profile/edit",UserAuth, async(req, res)=>{
         await loggedInUser.save();
 
         res.json({message : `${loggedInUser.firstName}, your profile is updated successfully!!`, data:loggedInUser})
+    } catch (error) {
+        console.error(error.message);
+        res.send("something went wrong " + error.message)
+    }
+})
+
+profileRouter.patch("/profile/changePassword",UserAuth, async(req, res)=>{
+    try {
+        const {oldPassword, newPassword} = req.body;
+
+        const user = req.user;
+
+        const isPasswordValid = await user.validatePassword(oldPassword)
+
+        if (!isPasswordValid) {
+            throw new Error("OLd password is incorrect!!");
+        }
+
+        user.password = await bcript.hash(newPassword, 10);
+
+        await user.save();
+
+        res.send("password change successfully!!!")
+
+       
     } catch (error) {
         console.error(error.message);
         res.send("something went wrong " + error.message)
