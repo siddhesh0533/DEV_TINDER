976afe26ed5a98dcdc07218afba4c56c459edb63
Siddhesh <siddheshpatil0533@gmail.com>
2025-10-08 00:50:27 +0530

    mongoDB connection string modify

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectdb =async()=>{
-    await mongoose.connect("mongodb+srv://siddheshpatil0533:sid0533@cluster0.5ce4j9m.mongodb.net/")
+    await mongoose.connect("mongodb+srv://siddheshpatil0533:sid0533@cluster0.5ce4j9m.mongodb.net/dev-tinder")
 } 
 
 module.exports = {connectdb}
