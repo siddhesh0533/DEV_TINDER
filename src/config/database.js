de669cd8bb21ad6812e86878a36dc42d415779dd
Siddhesh <siddheshpatil0533@gmail.com>
2025-10-08 00:26:17 +0530

    mongoDBconnection string change

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectdb =async()=>{
-    await mongoose.connect("mongodb://localhost:27017/devTinder")
+    await mongoose.connect("mongodb+srv://siddheshpatil0533_db_user:2wTcYypcpANGv2hC@cluster0.odcr7k2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 } 
 
 module.exports = {connectdb}
